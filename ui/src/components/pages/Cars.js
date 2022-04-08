2474ac4d7c27a493dee76bc2a43c518baaef4c2c
Andi Dika <andi.dika@student.uni-pr.edu>
2022-04-08 18:53:05 +0200

    Fix location start date end date search

--- ui/src/components/pages/Cars.js
+++ ui/src/components/pages/Cars.js
@@ -1,9 +1,46 @@
-import { Container, Row, Col, Button } from "react-bootstrap";
+import { Container, Row, Col, Form, Button} from "react-bootstrap";
 
 const Cars = () => {
   return (
-    <div></div>
-  )
-}
+    <div>
+      <Container className="mt-5">
+        <Form>
+          <Row>
+            <Col>
+              <Form.Control placeholder="Enter City" />
+            </Col>
+            <Col>
+              <Form.Group controlId="dob">
+                <Form.Control
+                  type="date"
+                  name="dob"
+                  placeholder="Date of Birth"
+                />
+              </Form.Group>
+            </Col>
+            <Col>
+              <Form.Group controlId="dob">
+                <Form.Control
+                  type="date"
+                  name="dob"
+                  placeholder="Date of Birth"
+                />
+              </Form.Group>
+            </Col>
+            <Col>
+            <div className="d-grid gap-2">
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </div>
+            </Col>
+          </Row>
+        </Form>
+      </Container>
+    </div>
 
-export default Cars
+    
+  );
+};
+
+export default Cars;
