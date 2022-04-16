8279b55f69396df6f769eb9c35b93c0451a8d7e3
Andi Dika <andi.dika@student.uni-pr.edu>
2022-04-17 00:34:15 +0200

    add atributes

---
+++ ui/src/components/CarCard.js
@@ -0,0 +1,74 @@
+import React from "react";
+import { Row, Col, Card, Container, Button, ListGroup } from "react-bootstrap";
+import {
+  FaCheckCircle,
+  FaCity,
+  FaShoppingBag,
+  FaSnowflake,
+  FaTimesCircle,
+  FaTools,
+  FaUserAlt,
+} from "react-icons/fa";
+import {Link} from 'react-router-dom'
+import sample1 from "../images/sample1.jpeg";
+
+const CarCard = ({ car }) => {
+  return (
+    <Col className="mt-2" md={4}>
+      <Card style={{ cursor: "pointer", textDecoration:"none", color:"black" }} as={Link} to={`${car._id}` } >
+        <Card.Img variant="top" src={sample1} />
+        <Card.Body>
+          <Card.Title>
+            {car.brand} {car.series}
+          </Card.Title>
+          <Card.Text className="icons">
+            <FaUserAlt />
+            <span>
+              <FaShoppingBag />
+            </span>
+            <span>
+              <FaCity />
+            </span>
+            <span>
+              <FaTools />
+            </span>
+            <span>
+              <FaSnowflake />
+            </span>
+          </Card.Text>
+          <Card.Text>
+            Transsmision: {car.transmission}
+            <FaCheckCircle color="royalblue" />
+          </Card.Text>
+          <Card.Text>
+            Air conditioning:
+            {car.air_conditioning ? (
+              <FaCheckCircle color="royalblue" />
+            ) : (
+              <FaTimesCircle color="royalblue" />
+            )}
+          </Card.Text>
+          <Card.Text>
+            <span style={{ color: "grey" }}>{car.car_type}</span>
+          </Card.Text>
+        </Card.Body>
+        <ListGroup className="list-group-flush">
+          <Card.Body>
+            <Container>
+              <Row>
+                <Col>
+                  <h3>${car.price_for_24h}</h3>
+                </Col>
+                <Col className="float-right">
+                  <Button>Book now!</Button>
+                </Col>
+              </Row>
+            </Container>
+          </Card.Body>
+        </ListGroup>
+      </Card>
+    </Col>
+  );
+};
+
+export default CarCard;
---
+++ ui/src/components/pages/Car.js
@@ -0,0 +1,35 @@
+import { useParams } from "react-router-dom";
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import { Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { FaArrowCircleLeft } from "react-icons/fa";
+
+const Car = () => {
+  let { id } = useParams();
+  const cars = useSelector((state) => state.cars);
+
+  const [car, setCar] = useState(cars.filter((car) => car._id === id));
+
+  console.log(car);
+
+  return (
+    <Container className="text-center">
+      <h1>
+        {car[0].car_model} {car[0].car_series}
+      </h1>
+      <p>{car[0].country}</p>
+      <p>{car[0].car_type}</p>
+      <p>{car[0].prod_year}</p>
+      <p>{id}</p>
+      <h1>
+        Back
+        <Link style={{ color: "black", textDecoration: "none" }} to="/cars">
+          <FaArrowCircleLeft />
+        </Link>
+      </h1>
+    </Container>
+  );
+};
+
+export default Car;
