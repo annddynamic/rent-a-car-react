0a2c5f235554e20cbff292a663ce4209d11d86f1
Andi Dika <andi.dika@student.uni-pr.edu>
2022-04-17 14:23:04 +0200

    added controllers to handle each request, code refactor

--- server/routes/cars.js
+++ server/routes/cars.js
@@ -1,82 +1,21 @@
+const CarsController = require('../controllers/cars')
 const router = require("express").Router();
-const Car = require("../models/car");
-	
-//GET ALL CARS
-router.get("/" , async (req, res) => {
-	try {
-		const cars = await Car.find();
-        res.json(cars);
 
-	} catch (error) {
-		res.json({ message: error});
-	}
-});
+// 		**** ALL ROUTES ARE PROTECTED*****
+
+//GET ALL CARS
+router.get("/", CarsController.get_all);
 
 //GET ONE SPECIFIC CAR
-router.get("/:carId", async(req,res) => {
-	// console.log(req.params.carId);
-	try{
-		const specificCar = await Car.findById(req.params.carId);
-		res.json(specificCar);
-	}catch (error) {
-		res.json({ message: error });
-	}
-
-});
+router.get("/:carId", CarsController.get_by_id);
 
 //DELETE SPECIFIC CAR
-router.delete("/:carId", async(req,res) =>{
-	try{
-		const deletedCar = await Car.findByIdAndDelete(req.params.carId)
-		res.json(deletedCar);
-	}catch (error) {
-		res.json({ message: error });
-	}
-});
+router.delete("/:carId", CarsController.delete_by_id);
 
 //UPDATE SPECIFIC CAR
-router.patch("/:carId", async(req,res) =>{
-	try{
-		const updatedCar = await Car.findByIdAndUpdate(
-			req.params.carId,
-			req.body,
-			{runValidators: true }
-	
-		);
-		res.json(updatedCar);
-	}catch (error) {
-		res.json({ message: error });
-	}
-});
+router.patch("/:carId", CarsController.update_by_id);
 
 // ADD A CAR TO DB
-router.post("/", async (req, res) => {
-	
-	// console.log(req.body)
-	const car = new Car({
-		car_model: req.body.car_model,
-		car_series :req.body.car_series,
-		car_type:req.body.car_type,
-		prod_year: req.body.prod_year,
-		price_for_24h: req.body.price_for_24h,
-		country: req.body.country,
-		seats:req.body.seats,
-		transmission: req.body.transmission,
-		max_speed: req.body.max_speed,
-		doors: req.body.doors,
-		air_conditioning: req.body.air_conditioning,
-		start_day_booking: req.body.start_day_booking,
-		finish_day_booking: req.body.finish_day_booking,
-		miles: req.body.miles
-	});
-
-	try{
-		const savedCar = await car.save();
-		res.json(savedCar)
-
-	}catch(error){
-		res.json({message:error});
-	}
-});
+router.post("/", CarsController.add_car);
 
 module.exports = router;
