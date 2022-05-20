a5a850393b5d4b4820f88322cef02ed1ace2a703
Nite Qela <nite.qela@student.uni-pr.edu>
2022-05-20 11:18:04 +0200

    Add chat to dashboard

--- ui/src/components/pages/Dashboard.js
+++ ui/src/components/pages/Dashboard.js
@@ -2,6 +2,7 @@ import {Container, Col, Row, Nav, Navbar} from 'react-bootstrap'
 import { useSelector } from 'react-redux'
 import './Dashboard.css'
 import pic from '../../images/babloki.jpg'
+import OnlineUsers from '../../chat/OnlineUsers'
 
 const Dashboard = () => {
   
@@ -11,30 +12,33 @@ const Dashboard = () => {
     <div>
         <Container className='mt-5'>
             <Row>
-              <Col className='text-center border-1' md={5}>
+              <Col className='text-center border-1' md={4}>
                   <div>
                     <img src={pic} style={{height:"26vh",width:"26vh", borderRadius:"50%"}} alt="" />
                   </div>
               </Col>
-              <Col md={7}>
+              <Col md={5}>
                   <h2>{user.firstName} {user.lastName}</h2>     
                   <h3 className='text-primary'>Web developer</h3>
                   <p className='text-secondary'>Lorem ipsum dolor sit amet.</p>
               </Col>
+              <Col className="border h-100" md={3}>
+                <OnlineUsers/>
+              </Col>
             </Row>
             <Row>
-              <Col className='text-center border-1' md={5}>
+              <Col className='text-center border-1' md={4}>
                   <h1>Work links</h1>     
               </Col>
-              <Col md={7}>
-              <Navbar bg="light" variant="light">
-                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-                <Nav className="me-auto">
-                  <Nav.Link href="#home">Home</Nav.Link>
-                  <Nav.Link href="#features">Features</Nav.Link>
-                  <Nav.Link href="#pricing">Pricing</Nav.Link>
-                </Nav>
-              </Navbar>
+              <Col md={5}>
+                <Navbar bg="light" variant="light">
+                  <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                  <Nav className="me-auto">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#features">Features</Nav.Link>
+                    <Nav.Link href="#pricing">Pricing</Nav.Link>
+                  </Nav>
+                </Navbar>
               </Col>
             </Row>
         </Container>
